f15e4ff96f20bfa64202f8b86086d45a36f6aa7a
Ted Spence <ted@spence.net>
2023-07-25 18:46:16 -0700

    Fix merge conflicts, add odata test

--- csharp/PmTask/Program.cs
+++ csharp/PmTask/Program.cs
@@ -1,232 +1,240 @@
 ﻿using System.Reflection;
 using CommandLine;
-using PmTask;
 using ProjectManager.SDK;
 using ProjectManager.SDK.Models;
 using Simple.OData.Client;
 
-public static class Program
+namespace PmTask
 {
-    private class BaseOptions
+    public static class Program
     {
-        [Option('a', "apikey", HelpText = "The API key to use.  If not specified, uses the environment variable PM_API_KEY.")]
-        public string? ApiKey { get; set; }
-        
-        [Option('e', "env", HelpText = "The URL of the ProjectManager environment to use.  If not specified, uses the environment variable PM_ENV.")]
-        public string? Env { get; set; }
-    }
-    
-    [Verb("create", HelpText = "Create a new task")]
-    private class CreateOptions : BaseOptions
-    {
-        [Option("project", HelpText = "The name, ID, or ShortID of the project in which to create the task")]
-        public string? Project { get; set; }
-        
-        [Option("name", HelpText = "Name of the task")]
-        public string? TaskName { get; set; }
-        
-        [Option("desc", HelpText = "Description of the task")]
-        public string? Description { get; set; }
-        
-        [Option("pri", HelpText = "Priority of the task")]
-        public string? Priority { get; set; }
-    }
-    
-    [Verb("list", HelpText = "List tasks within a project")]
-    private class ListOptions : BaseOptions
-    {
-        [Option("project", HelpText = "The name, ID, or ShortID of the project in which to create the task")]
-        public string? Project { get; set; }
-    }
-    
-    [Verb("discuss", HelpText = "Read all discussion comments about a task")]
-    private class DiscussOptions : BaseOptions
-    {
-        [Option("task", HelpText = "The ShortID of the task to review discussion")]
-        public string? Task { get; set; }
-    }
-    
-    [Verb("comment", HelpText = "Add a comment to a task within a project")]
-    private class CommentOptions : BaseOptions
-    {
-        [Option("task", HelpText = "The ShortID of the task to comment upon")]
-        public string? Task { get; set; }
-        
-        [Option("message", HelpText = "The text, in markdown format, to add to the discussion")]
-        public string? Message { get; set; }
-    }
-
-    [Verb("odata", HelpText = "Test OData connectivity")]
-    private class OdataOptions : BaseOptions
-    {
-        
-    }
-    
-    private	static Type[] LoadVerbs()
-    {
-        return Assembly.GetExecutingAssembly().GetTypes()
-            .Where(t => t.GetCustomAttribute<VerbAttribute>() != null).ToArray();		 
-    }
-    
-    public static void Main(string[] args)
-    {
-        var types = LoadVerbs();	
-        Parser.Default.ParseArguments(args, types)
-            .WithParsed<CreateOptions>(CreateTask)
-            .WithParsed<ListOptions>(ListTask)
-            .WithParsed<DiscussOptions>(DiscussTask)
-            .WithParsed<CommentOptions>(CommentTask)
-            .WithParsed<OdataOptions>(OdataTask)
-            .WithNotParsed(HandleErrors);
-    }
-
-    private static void OdataTask(OdataOptions options)
-    {
-        var client = MakeOdataClient(options);
-        var projects = client.Filter(item => item.Name == "test").FindEntriesAsync().Result.ToList();
-        foreach (var project in projects)
+        private class BaseOptions
         {
-            Console.WriteLine($"Project fetched from OData: {project.Name}");
-        }
-    }
+            [Option('a', "apikey",
+                HelpText = "The API key to use.  If not specified, uses the environment variable PM_API_KEY.")]
+            public string? ApiKey { get; set; }
 
-    private static void DiscussTask(DiscussOptions options)
-    {
-        // Fetch project and task
-        var client = MakeClient(options);
-        var task = client.FindTask(options.Task).Result;
-        if (task?.Id == null)
-        {
-            return;
+            [Option('e', "env",
+                HelpText =
+                    "The URL of the ProjectManager environment to use.  If not specified, uses the environment variable PM_ENV.")]
+            public string? Env { get; set; }
         }
 
-        Console.WriteLine($"Task {task.Name} ({task.ShortId})");
-        
-        // Retrieve discussions
-        var discussions = client.Discussion.RetrieveTaskComments(task.Id.Value).Result;
-        if (discussions.Data.Length == 0)
+        [Verb("create", HelpText = "Create a new task")]
+        private class CreateOptions : BaseOptions
         {
-            Console.WriteLine("No comments.");
+            [Option("project", HelpText = "The name, ID, or ShortID of the project in which to create the task")]
+            public string? Project { get; set; }
+
+            [Option("name", HelpText = "Name of the task")]
+            public string? TaskName { get; set; }
+
+            [Option("desc", HelpText = "Description of the task")]
+            public string? Description { get; set; }
+
+            [Option("pri", HelpText = "Priority of the task")]
+            public string? Priority { get; set; }
         }
-        foreach (var comment in discussions.Data)
+
+        [Verb("list", HelpText = "List tasks within a project")]
+        private class ListOptions : BaseOptions
         {
-            Console.WriteLine($"On {comment.CreateDate} {comment.AuthorName} wrote:");
-            Console.WriteLine("  " + comment.Text);
-            foreach (var reaction in comment.Emoji ?? Array.Empty<DiscussionEmoji>())
+            [Option("project", HelpText = "The name, ID, or ShortID of the project in which to create the task")]
+            public string? Project { get; set; }
+        }
+
+        [Verb("discuss", HelpText = "Read all discussion comments about a task")]
+        private class DiscussOptions : BaseOptions
+        {
+            [Option("task", HelpText = "The ShortID of the task to review discussion")]
+            public string? Task { get; set; }
+        }
+
+        [Verb("comment", HelpText = "Add a comment to a task within a project")]
+        private class CommentOptions : BaseOptions
+        {
+            [Option("task", HelpText = "The ShortID of the task to comment upon")]
+            public string? Task { get; set; }
+
+            [Option("message", HelpText = "The text, in markdown format, to add to the discussion")]
+            public string? Message { get; set; }
+        }
+
+        [Verb("odata", HelpText = "Test OData connectivity")]
+        private class OdataOptions : BaseOptions
+        {
+
+        }
+
+        private static Type[] LoadVerbs()
+        {
+            return Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => t.GetCustomAttribute<VerbAttribute>() != null).ToArray();
+        }
+
+        public static void Main(string[] args)
+        {
+            var types = LoadVerbs();
+            Parser.Default.ParseArguments(args, types)
+                .WithParsed<CreateOptions>(CreateTask)
+                .WithParsed<ListOptions>(ListTask)
+                .WithParsed<DiscussOptions>(DiscussTask)
+                .WithParsed<CommentOptions>(CommentTask)
+                .WithParsed<OdataOptions>(OdataTask)
+                .WithNotParsed(HandleErrors);
+        }
+
+        private static void OdataTask(OdataOptions options)
+        {
+            var client = MakeOdataClient(options);
+            var projects = client.Filter(item => item.Name == "test").FindEntriesAsync().Result.ToList();
+            foreach (var project in projects)
             {
-                Console.WriteLine($"Reaction: {reaction.Name} ({reaction.UserIds.Length})");
+                Console.WriteLine($"Project fetched from OData: {project.Name}");
             }
         }
-    }
-    
-    
-    private static void CommentTask(CommentOptions options)
-    {
-        // Fetch project and task
-        var client = MakeClient(options);
-        var task = client.FindTask(options.Task).Result;
-        if (task?.Id == null)
-        {
-            return;
-        }
 
-        // Explain what we're doing
-        Console.WriteLine($"Task {task.Name} ({task.ShortId})");
-        
-        // Add discussion
-        var item = new DiscussionCreateDto()
+        private static void DiscussTask(DiscussOptions options)
         {
-            Text = options.Message,
-        };
-        var result = client.Discussion.CreateTaskComments(task.Id.Value, item).Result;
-        if (result == null || !result.Success)
-        {
-            Console.WriteLine("Task comment failed.");
-            return;
-        }
-
-        Console.WriteLine($"Added discussion comment {result.Data.DiscussionCommentId}.");
-    }
-
-    private static void ListTask(ListOptions options)
-    {
-        var client = MakeClient(options);
-        var project = client.FindProject(options.Project).Result;
-        if (project == null)
-        {
-            return;
-        }
-
-        // Print out information about this project
-        Console.WriteLine($"Project {project.Name} ({project.ShortId})");
-        
-        // List all tasks within this project
-        var tasks = client.Task.QueryTasks(null, null, $"projectId eq {project.Id}", null, null, null).Result;
-        foreach (var task in tasks.Data)
-        {
-            Console.WriteLine($"  {task.ShortId} {task.Name} ({task.PercentComplete}% complete)");
-        }
-
-        Console.WriteLine($"  {tasks.Data.Length} tasks.");
-    }
-    private static void HandleErrors(IEnumerable<Error> errors)
-    {
-        var errList = errors.ToList();
-        Console.WriteLine($"Found {errList.Count} errors.");
-    }
-
-    private static void CreateTask(CreateOptions options)
-    {
-        var client = MakeClient(options);
-        
-        // Fetch all projects and find the one that matches locally so we can give debugging information
-        var project = client.FindProject(options.Project).Result;
-        if (project?.Id == null)
-        {
-            return;
-        }
-        
-        // Create a task for this project
-        var info = new TaskCreateDto()
-        {
-            Name = options.TaskName,
-            Description = options.Description,
-        };
-        var task = client.Task.CreateTask(project.Id.Value, info).Result;
-        if (!task.Success)
-        {
-            Console.WriteLine($"Could not create task {options.TaskName}: {task.Error.Message}");
-            return;
-        }
-        Console.WriteLine($"Created task {options.TaskName}: {task.Data.Id}");
-    }
-
-    private static IBoundClient<ProjectDto> MakeOdataClient(BaseOptions options)
-    {
-        var apiKey = options.ApiKey ?? Environment.GetEnvironmentVariable("PM_API_KEY");
-        var env = options.ApiKey ?? Environment.GetEnvironmentVariable("PM_ENV");
-
-        var oDataClientSettings = new ODataClientSettings(new Uri(env + "/project-api/public/projects"))
-        {
-            BeforeRequest = delegate(HttpRequestMessage message)
+            // Fetch project and task
+            var client = MakeClient(options);
+            var task = client.FindTask(options.Task).Result;
+            if (task?.Id == null)
             {
-                message.Headers.Add("Authorization", "Bearer " + apiKey);
-            },
-            OnTrace = Console.WriteLine,
-            IgnoreResourceNotFoundException = true,
-        };
+                return;
+            }
 
-        var mainClient = new ODataClient(oDataClientSettings);
-        return mainClient.For<ProjectDto>();
-    }
-    
-    private static ProjectManagerClient MakeClient(BaseOptions options)
-    {
-        var apiKey = options.ApiKey ?? Environment.GetEnvironmentVariable("PM_API_KEY");
-        var env = options.Env ?? Environment.GetEnvironmentVariable("PM_ENV");
-        return ProjectManagerClient
-            .WithCustomEnvironment(env)
-            .WithBearerToken(apiKey)
-            .WithAppName("PmTask");
+            Console.WriteLine($"Task {task.Name} ({task.ShortId})");
+
+            // Retrieve discussions
+            var discussions = client.Discussion.RetrieveTaskComments(task.Id.Value).Result;
+            if (discussions.Data.Length == 0)
+            {
+                Console.WriteLine("No comments.");
+            }
+
+            foreach (var comment in discussions.Data)
+            {
+                Console.WriteLine($"On {comment.CreateDate} {comment.AuthorName} wrote:");
+                Console.WriteLine("  " + comment.Text);
+                foreach (var reaction in comment.Emoji ?? Array.Empty<DiscussionEmoji>())
+                {
+                    Console.WriteLine($"Reaction: {reaction.Name} ({reaction.UserIds.Length})");
+                }
+            }
+        }
+
+
+        private static void CommentTask(CommentOptions options)
+        {
+            // Fetch project and task
+            var client = MakeClient(options);
+            var task = client.FindTask(options.Task).Result;
+            if (task?.Id == null)
+            {
+                return;
+            }
+
+            // Explain what we're doing
+            Console.WriteLine($"Task {task.Name} ({task.ShortId})");
+
+            // Add discussion
+            var item = new DiscussionCreateDto()
+            {
+                Text = options.Message,
+            };
+            var result = client.Discussion.CreateTaskComments(task.Id.Value, item).Result;
+            if (result == null || !result.Success)
+            {
+                Console.WriteLine("Task comment failed.");
+                return;
+            }
+
+            Console.WriteLine($"Added discussion comment {result.Data.DiscussionCommentId}.");
+        }
+
+        private static void ListTask(ListOptions options)
+        {
+            var client = MakeClient(options);
+            var project = client.FindProject(options.Project).Result;
+            if (project == null)
+            {
+                return;
+            }
+
+            // Print out information about this project
+            Console.WriteLine($"Project {project.Name} ({project.ShortId})");
+
+            // List all tasks within this project
+            var tasks = client.Task.QueryTasks(null, null, $"projectId eq {project.Id}", null, null, null).Result;
+            foreach (var task in tasks.Data)
+            {
+                Console.WriteLine($"  {task.ShortId} {task.Name} ({task.PercentComplete}% complete)");
+            }
+
+            Console.WriteLine($"  {tasks.Data.Length} tasks.");
+        }
+
+        private static void HandleErrors(IEnumerable<Error> errors)
+        {
+            var errList = errors.ToList();
+            Console.WriteLine($"Found {errList.Count} errors.");
+        }
+
+        private static void CreateTask(CreateOptions options)
+        {
+            var client = MakeClient(options);
+
+            // Fetch all projects and find the one that matches locally so we can give debugging information
+            var project = client.FindProject(options.Project).Result;
+            if (project?.Id == null)
+            {
+                return;
+            }
+
+            // Create a task for this project
+            var info = new TaskCreateDto()
+            {
+                Name = options.TaskName,
+                Description = options.Description,
+            };
+            var task = client.Task.CreateTask(project.Id.Value, info).Result;
+            if (!task.Success)
+            {
+                Console.WriteLine($"Could not create task {options.TaskName}: {task.Error.Message}");
+                return;
+            }
+
+            Console.WriteLine($"Created task {options.TaskName}: {task.Data.Id}");
+        }
+
+        private static IBoundClient<ProjectDto> MakeOdataClient(BaseOptions options)
+        {
+            var apiKey = options.ApiKey ?? Environment.GetEnvironmentVariable("PM_API_KEY");
+            var env = options.ApiKey ?? Environment.GetEnvironmentVariable("PM_ENV");
+
+            var oDataClientSettings = new ODataClientSettings(new Uri(env + "/project-api/public/projects"))
+            {
+                BeforeRequest = delegate(HttpRequestMessage message)
+                {
+                    message.Headers.Add("Authorization", "Bearer " + apiKey);
+                },
+                OnTrace = Console.WriteLine,
+                IgnoreResourceNotFoundException = true,
+            };
+
+            var mainClient = new ODataClient(oDataClientSettings);
+            return mainClient.For<ProjectDto>();
+        }
+
+        private static ProjectManagerClient MakeClient(BaseOptions options)
+        {
+            var apiKey = options.ApiKey ?? Environment.GetEnvironmentVariable("PM_API_KEY");
+            var env = options.Env ?? Environment.GetEnvironmentVariable("PM_ENV");
+            return ProjectManagerClient
+                .WithCustomEnvironment(env)
+                .WithBearerToken(apiKey)
+                .WithAppName("PmTask");
+        }
     }
 }
