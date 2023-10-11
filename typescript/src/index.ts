aa95c54b607f769dee25ee9788618d2e5edac843
Ted Spence <ted@spence.net>
2023-10-11 13:24:07 +1300

    Tinkering with example program

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,24 +1,26 @@
 import * as process from 'process';
-import * as projectmanager from 'projectmanager-sdk';
-
-// Fetch the API key from an environment variable
-console.log('Checking environment variables for ProjectManager API key.');
-var apiKey = process.env["PM_API_KEY"] || "Fill in your API key here";
-if (!apiKey) {
-    console.log('Please specify an API key in the environment variable PM_API_KEY.');
-    process.exit();
-}
-
-// Create a client
-var client = projectmanager.ProjectManagerClient
-    .withEnvironment("production")
-    .withApiKey(apiKey);
+import { ProjectManagerClient } from 'projectmanager-sdk';
 
 async function DemonstrationMethod()
 {
+    // Fetch the API key from an environment variable
+    console.log('Checking environment variables for ProjectManager API key.');
+    var apiKey = process.env["PM_API_KEY"] || "Fill in your API key here";
+    if (!apiKey) {
+        console.log('Please specify an API key in the environment variable PM_API_KEY.');
+        process.exit();
+    }
+
+    // Create a client
+    var client = ProjectManagerClient
+        .withEnvironment("production")
+        .withApiKey(apiKey);
+
     // Check that we are connected
+    console.log('About to call retrieve me');
     var result = await client.Me.retrieveMe();
     console.log(`We are connected as ${result.data?.fullName} (${result.data?.emailAddress}`);
 }
 
-DemonstrationMethod();
+DemonstrationMethod()
+    .then(result => console.log(`Finished: ${result}`));
