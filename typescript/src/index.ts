54a2cbd44ff1b3931640e6c489b61cc862b549ee
Ted Spence <ted@spence.net>
2023-10-11 15:17:17 +1300

    Add TypeScript example program (#6)
    * First attempt at a typescript example
    
    * Looks like this executes
    
    * Tinkering with example program
    
    * Tinkering
    
    * Switch build approach
    
    * Alex is a genius

---
+++ typescript/src/index.ts
@@ -0,0 +1,32 @@
+import * as process from 'process';
+import { ProjectManagerClient } from 'projectmanager-sdk';
+
+async function DemonstrationMethod()
+{
+    // Fetch the API key from an environment variable
+    console.log('Checking environment variables for ProjectManager API key.');
+    var apiKey = process.env["PM_API_KEY"] || "Fill in your API key here";
+    if (!apiKey) {
+        console.log('Please specify an API key in the environment variable PM_API_KEY.');
+        process.exit();
+    }
+
+    // Create a client
+    var client = ProjectManagerClient
+        .withEnvironment("production")
+        .withBearerToken(apiKey);
+
+    // Check that we are connected
+    console.log('About to call retrieve me');
+    var result = await client.Me.retrieveMe();
+    console.log(`We are connected as ${result.data?.fullName} (${result.data?.emailAddress})`);
+
+    // Fetch tasks
+    var tasks = await client.Task.queryTasks(null, null, null, null, null, null);
+    tasks.data.forEach(task => {
+        console.log(`Task ${task.shortId} - ${task.name}`);
+    });
+}
+
+DemonstrationMethod()
+    .then(result => console.log(`Finished: ${result}`));
