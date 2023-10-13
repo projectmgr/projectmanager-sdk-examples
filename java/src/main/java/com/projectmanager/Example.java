47bbc9709c30f7837be40fa00f8142e36e9e9b8b
Ted Spence <ted@spence.net>
2023-10-13 16:01:31 +1300

    Hacking together a java console app

---
+++ java/src/main/java/com/projectmanager/Example.java
@@ -0,0 +1,19 @@
+package com.projectmanager;
+
+/**
+ * Starter class for MyStats application.
+ *
+ * @author janbodnar
+ */
+
+public class Example {
+
+    /**
+     * Application entry point.
+     *
+     * @param args application command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
