6eab5f1c8ec55c037d5b6b59d28bb021a9b8e314
Ted Spence <ted@spence.net>
2023-10-14 14:09:06 +1300

    Example Java application (#8)
    * Hacking together a java console app
    
    * Keep on tinkering
    
    * SO MUCH tinkering
    
    * Work on fixing references
    
    * Example app finally works
    
    * Print out tasks

---
+++ java/src/main/java/com/example/ExampleApp.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import com.projectmanager.ProjectManagerClient;
+
+/**
+ * Starter class for MyStats application.
+ *
+ * @author janbodnar
+ */
+
+public class ExampleApp {
+
+    /**
+     * Application entry point.
+     *
+     * @param args application command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+
+        // Retrieve API key from the environment variable
+        String key = System.getenv().getOrDefault("PM_API_KEY", null);
+
+        // Construct a client to talk to the ProjectManager API
+        ProjectManagerClient client = ProjectManagerClient
+            .withEnvironment("production")
+            .withBearerToken(key);
+
+        // Check server connectivity
+        var result = client.getMeClient().retrieveMe();
+        if (result.getSuccess()) {
+            System.out.println("You are logged on as " + result.getData().getFullName() + " (" + result.getData().getEmailAddress() + ")");
+        } else {
+            System.out.println("Failed to connect to the server.  Error: " + result.getError().getMessage());
+        }
+
+        // Retrieve tasks
+        var tasks = client.getTaskClient().queryTasks(null, null, null, null, null, null);
+        for (var task : tasks.getData()) {
+            System.out.println("Task " + task.getShortId() + " - " + task.getName());
+        }
+    }
+}
