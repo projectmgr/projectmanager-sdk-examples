093ad0dda893485f0cf5ddd02afeea2e3da1151d
Ted Spence <ted@spence.net>
2024-01-29 14:56:49 -0800

    Update python examples (#11)
    Update example to demonstrate project creation.

--- python/python_task_list.py
+++ python/python_task_list.py
@@ -1,5 +1,12 @@
 import os
+
+# To test Python locally, uncomment these lines:
+# import sys
+# sys.path.append('/github/projectmanager-sdk-python/src')
+
 import ProjectManagerSdk
+import dataclasses
+import json
 
 def retrieve_api_key():
     API_KEY = os.environ.get('PM_API_KEY')
@@ -19,6 +26,18 @@ def create_client(apikey):
     else:
         return client
 
+def remove_empty_elements(d):
+    """recursively remove empty lists, empty dicts, or None elements from a dictionary"""
+
+    def empty(x):
+        return x is None or x == {} or x == []
+
+    if not isinstance(d, (dict, list)):
+        return d
+    elif isinstance(d, list):
+        return [v for v in (remove_empty_elements(v) for v in d) if not empty(v)]
+    else:
+        return {k: v for k, v in ((k, remove_empty_elements(v)) for k, v in d.items()) if not empty(v)}
 
 def main():
     API_KEY = retrieve_api_key()
@@ -30,7 +49,8 @@ def main():
         exit()
     print(f"Logged in as {status_results.data.fullName} ({status_results.data.emailAddress})")
 
-    tasks = client.task.query_tasks(None, None, None, None, None, None)
+    # Demonstrate querying tasks
+    tasks = client.task.query_tasks(None, None, None, None, None)
 
     if tasks.data == None or len(tasks.data) == 0:
         print("No records found matching this query.")
@@ -41,7 +61,12 @@ def main():
         print(f"Task {count}: {task.shortId} {task.name}")
         count += 1
 
-
+    # Demonstrate creating a project
+    new_project = ProjectManagerSdk.ProjectCreateDto()
+    new_project.name = "New Project"
+    new_project.description = "This is my project description"
+    result = client.project.create_project(new_project)
+    print(f"Result: {result}")
 
 if __name__ == '__main__':
     main()
